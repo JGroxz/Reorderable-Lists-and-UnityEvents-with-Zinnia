bd0f66464133d9a93fe5786dd950bda41455d2d0
Yevhen Bondarenko <zekbond@gmail.com>
2020-08-25 13:20:43 +0300

    Add initial implementation of reorderable UnityEvents with Zinnia

--- Assets/Scripts/ReorderableUnityEventsTest.cs
+++ Assets/Scripts/ReorderableUnityEventsTest.cs
@@ -1,9 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Malimbe.PropertySerializationAttribute;
 using UnityEngine;
 using UnityEngine.Events;
+using Zinnia.Action;
 
 public class ReorderableUnityEventsTest : MonoBehaviour
 {
     public UnityEvent testEvent = new UnityEvent();
+    
+    public FloatAction.UnityEvent floatEvent = new FloatAction.UnityEvent();
+    
+    public Vector3Action.UnityEvent vector3Event = new Vector3Action.UnityEvent();
+    
+    [Serialized]
+    public UnityEvent malimbeSerializedEvent { get; set; } = new UnityEvent();
+
+    public void DummyMethod() => Debug.Log("DummyMethod called.");
+    
+    public void DummyFloatMethod(float value) => Debug.Log($"DummyFloatMethod called (value = {value}).");
+    
+    public void DummyVector3Method(Vector3 value) => Debug.Log($"DummyVector3Method called (value = {value}).");
 }
